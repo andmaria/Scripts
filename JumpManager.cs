21fa27f49ab9d00b89fc10de25fed3e089e619d8
Мaria <33930033+andmaria@users.noreply.github.com>
2021-07-02 15:53:29 +0300

    Add files via upload

---
+++ JumpManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpManager : MonoBehaviour {
+
+    private Animation anim;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        anim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animation>();    
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();    
+    }
+
+    public void JumpRight()
+    {
+        if (!(anim.isPlaying))     
+        {
+            if (playerTransform.position.x == -3f)       
+                anim.Play("SecondRight");      
+
+            else if (playerTransform.position.x == 0f)     
+                anim.Play("ThirdRight");        
+        }
+    }
+
+    public void JumpLeft()
+    {
+        if (!(anim.isPlaying))      
+        {
+            if (playerTransform.position.x == 0f)      
+                anim.Play("FirstLeft");      
+
+            else if (playerTransform.position.x == 3f)     
+                anim.Play("SecondLeft");        
+        }
+    }
+}
